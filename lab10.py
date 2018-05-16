3deabe9c62185b97c136e2add4f4b95089dca7ac
Antonio Reyes <35511339+T0N1R@users.noreply.github.com>
2018-05-16 14:41:16 -0600

    Opciones 1,2,3,4,5,7
    Las opciones previamente mencionadas son funcionales
    dificultad para mostrar los doctores en opción 6

--- lab10.py
+++ lab10.py
@@ -23,26 +23,52 @@ def add_patient():
     dt = raw_input("Ingresa fecha de visita: ")
     medicine = raw_input("Ingresa medicina recetada: ")
     pat = db.nodes.create(name=nm, doctor=doc_name, date=dt, prescription=medicine)
-    med = db.nodes.create(prescription=medicine)
-    
     patient.add(pat)
+    pat.relationships.create("takes", meds.get(prescription=medicine)[0])
+    pat.relationships.create("visits", doctor.get(name=doc_name)[0])
+    doctor.get(name=doc_name)[0].relationships.create("prescribes",meds.get(prescription=medicine)[0])
+
+def add_medication():
+    nm = raw_input("Ingrese el nombre del medicamento: ")
+    med = db.nodes.create(prescription=nm)
     meds.add(med)
-    pat.relationships.create("takes", med)
 
 def knows():
     p1 = raw_input("Ingrese nombre de paciente 1: ")
     p2 = raw_input("Ingrese nombre de paciente 2: ")
     patient.get(name=p1)[0].relationships.create("knows",patient.get(name=p2)[0])
+    patient.get(name=p2)[0].relationships.create("knows",patient.get(name=p1)[0])
 
-#def doctor_appointment():
+def what_field():
+    opcion = raw_input("Ingrese la especialidad: ")
+    person = db.labels.get("Doctor")
+    return person.get(field=opcion)
+    #person = db.labels.doctor.get(field=opcion)
+    #person.all()
+    #print(doctor.get(field=opcion))
+
+def doctor_appointment():
+    doc = raw_input("Ingrese el nombre del doctor: ")
+    pat = raw_input("Ingrese el nombre del paciente: ")
+    patient.get(name=pat)[0].relationships.create("visits", doctor.get(name=doc)[0])
+
+def prescribe():
+    doc = raw_input("Ingrese el nombre del doctor: ")
+    pat = raw_input("Ingrese el nombre del paciente: ")
+    med = raw_input("Ingrese el nombre del medicamento: ")
+    patient.get(name=pat)[0].relationships.create("takes", meds.get(prescription=med)[0])
+    doctor.get(name=doc)[0].relationships.create("prescribes", meds.get(prescription=med)[0])
     
 
 def menu():
     print("1. Ingresar Doctor")
     print("2. Ingresar Paciente")
-    print("3. Ingresar doctor para paciente")
-    print("4. Consultar doctores con cierta especialidad")
-    print("5. Ingresar que una persona conoce a otra")
+    print("3. Ingresar Medicamento")
+    print("4. Ingresar doctor para paciente")
+    print("5. Recetar medicamento para paciente")
+    print("6. Consultar doctores con cierta especialidad")
+    print("7. Ingresar que una persona conoce a otra")
+    print("9. Salir")
 
 menu()
 opcion = input("Ingrese la acción a realizar: ")
@@ -59,17 +85,52 @@ while(opcion != 9):
         print ("**********************************")
         print ("**********************************")
 
-
-
     elif(opcion == 2):
         add_patient()
         print ("**********************************")
         print ("**********************************")
+        menu()
+        opcion = input("Ingrese la acción a realizar: ")
+        print ("**********************************")
+        print ("**********************************")
+
+    elif(opcion == 3):
+        add_medication()
+        print ("**********************************")
+        print ("**********************************")
+        menu()
+        opcion = input("Ingrese la acción a realizar: ")
+        print ("**********************************")
+        print ("**********************************")
+
+    elif(opcion == 4):
+        doctor_appointment()
+        print ("**********************************")
+        print ("**********************************")
+        menu()
         opcion = input("Ingrese la acción a realizar: ")
         print ("**********************************")
         print ("**********************************")
 
     elif(opcion == 5):
+        prescribe()
+        print ("**********************************")
+        print ("**********************************")
+        menu()
+        opcion = input("Ingrese la acción a realizar: ")
+        print ("**********************************")
+        print ("**********************************")
+        
+    elif(opcion == 6):
+        what_field()
+        print ("**********************************")
+        print ("**********************************")
+        menu()
+        opcion = input("Ingrese la acción a realizar: ")
+        print ("**********************************")
+        print ("**********************************")
+
+    elif(opcion == 7):
         knows()
         print ("**********************************")
         print ("**********************************")
@@ -78,8 +139,11 @@ while(opcion != 9):
         print ("**********************************")
         print ("**********************************")
 
-  #  elif(opcion == 3):
- #       doctor_appointment()
+    else:
+        print("La opción ingresada no es valida")
+        menu()
+        opcion = input("Ingrese la accion a realizar: ")
+
 
 print("Gracias por usar el programa")
 
